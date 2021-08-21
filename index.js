55404a6c5c5476b3eec592be30b5c5c6a8158931
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-21 16:09:13 +0430

    :sparkles: displayed simple html form for signup page

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hi there!");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="passoword" />
+        <input name="passwordConfirmation" placeholder="password confirmaition" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
+});
+
+app.post("/", (req, res) => {
+  res.send("Account created!!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on Port 3000");
+});
