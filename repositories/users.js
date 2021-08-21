319c3414244d1d57e6433f9238e7af173a2b1422
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-21 18:36:58 +0430

    :sparkles: made update method

--- repositories/users.js
+++ repositories/users.js
@@ -53,12 +53,21 @@ class UsersRepository {
     const filteredRecords = records.filter((record) => record.id !== id);
     await this.writeAll(filteredRecords);
   }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find((record) => record.id === id);
+
+    if (!record) throw new Error(`Record with id ${id} not found`);
+
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository("users.json");
-
-  await repo.delete("ee6a2e78");
+  await repo.update("f40cea3b", { password: "mypassword" });
 };
 
 test();
