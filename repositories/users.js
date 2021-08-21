365aa6faeed87a3f66554125f843da795a3f9842
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-21 18:19:28 +0430

    :sparkles: made randomId method that uses crypto module to generate randomIds

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const crypto = require("crypto");
 
 class UsersRepository {
   constructor(filename) {
@@ -23,6 +24,8 @@ class UsersRepository {
   }
 
   async create(attrs) {
+    attrs.id = this.randomId();
+
     const records = await this.getAll();
     records.push(attrs);
 
@@ -34,6 +37,10 @@ class UsersRepository {
       this.filename,
       JSON.stringify(records, null, 2)
     );
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString("hex");
   }
 }
 
