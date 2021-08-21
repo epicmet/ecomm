79a6d164bc848094973649623bf943050449b74f
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-21 18:57:28 +0430

    :hammer: exported new instance of UserRepository

--- repositories/users.js
+++ repositories/users.js
@@ -81,13 +81,4 @@ class UsersRepository {
   }
 }
 
-const test = async () => {
-  const repo = new UsersRepository("users.json");
-
-  const user = await repo.getOneBy({
-    thing: "0cea3b",
-  });
-  console.log(user);
-};
-
-test();
+module.exports = new UsersRepository("users.json");
