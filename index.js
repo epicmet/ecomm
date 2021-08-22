1f0f56bff2d722dd40515e973b7abd432855024e
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-22 18:35:45 +0430

    :hammer: exposed public folder for browser

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const authRouter = require("./routes/admin/auth");
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cookieSession({
