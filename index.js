ede1e791b8798284380278408ef76657b83a68f8
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-22 16:03:35 +0430

    :recycle: extracted auth route in its own file

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 // bodyParser is deprecated, instead I used express.urlencoded
 // const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
-const usersRepo = require("./repositories/users");
+const authRouter = require("./routes/admin/auth");
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cookieSession({
@@ -12,68 +13,7 @@ app.use(
   })
 );
 
-app.get("/signup", (req, res) => {
-  res.send(`
-    <div>
-      Your ID is: ${req.session.userId}
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="passoword" />
-        <input name="passwordConfirmation" placeholder="password confirmaition" />
-        <button>Sign Up</button>
-      </form>
-    </div>
-  `);
-});
-
-app.post("/signup", async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) return res.send("Email is in use!");
-
-  if (password !== passwordConfirmation)
-    return res.send("Password must match!");
-
-  const user = await usersRepo.create({ email, password });
-  req.session.userId = user.id;
-
-  res.send("Account created!!");
-});
-
-app.get("/signout", (req, res) => {
-  req.session = null;
-  res.send("You are logged out!");
-});
-
-app.get("/signin", (req, res) => {
-  res.send(`
-    <div>
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="passoword" />
-        <button>Sign In</button>
-      </form>
-    </div>
-  `);
-});
-
-app.post("/signin", async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await usersRepo.getOneBy({ email });
-  if (!user) return res.send("Email not found");
-
-  const validPassword = await usersRepo.comparePasswords(
-    user.password,
-    password
-  );
-  if (!validPassword) return res.send("Invalid password");
-
-  req.session.userId = user.id;
-
-  res.send("You are signed in!");
-});
+app.use(authRouter);
 
 app.listen(3000, () => {
   console.log("Listening on Port 3000");
