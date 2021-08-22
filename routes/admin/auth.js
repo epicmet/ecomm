87ecacd267c30f48f8e74063b66b4a052809980b
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-22 17:10:33 +0430

    :hammer: made authentication validation with express-validator middleware for signup

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
+
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
@@ -9,20 +11,45 @@ router.get("/signup", (req, res) => {
   res.send(signupTemplate({ req }));
 });
 
-router.post("/signup", async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
+router.post(
+  "/signup",
+  [
+    check("email")
+      .trim()
+      .normalizeEmail()
+      .isEmail()
+      .withMessage("Must be a valid email")
+      .custom(async (email) => {
+        const existingUser = await usersRepo.getOneBy({ email });
+        if (existingUser) {
+          throw new Error("Email is in use");
+        }
+      }),
+    check("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Must be between 4 and 20 characters"),
+    check("passwordConfirmation")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Must be between 4 and 20 characters")
+      .custom((passwordConfirmation, { req }) => {
+        if (passwordConfirmation !== req.body.password) {
+          throw new Error("Passwords must match");
+        }
+      }),
+  ],
+  async (req, res) => {
+    const { errors } = validationResult(req);
+    console.log(errors);
+    const { email, password, passwordConfirmation } = req.body;
 
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) return res.send("Email is in use!");
+    const user = await usersRepo.create({ email, password });
+    req.session.userId = user.id;
 
-  if (password !== passwordConfirmation)
-    return res.send("Password must match!");
-
-  const user = await usersRepo.create({ email, password });
-  req.session.userId = user.id;
-
-  res.send("Account created!!");
-});
+    res.send("Account created!!");
+  }
+);
 
 router.get("/signout", (req, res) => {
   req.session = null;
