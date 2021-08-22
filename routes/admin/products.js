f7df742e6c3763258e46bac2c435e30c46e7d2e5
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-22 19:26:18 +0430

    :hammer: made products form validation

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const { validationResult } = require("express-validator");
 
 const ProductsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
+const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
 
@@ -9,6 +11,13 @@ router.get("/admin/products", (req, res) => {});
 
 router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
+});
+
+router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
+  const errors = validationResult(req);
+  console.log(errors);
+
+  res.send("submitted");
 });
 
 module.exports = router;
