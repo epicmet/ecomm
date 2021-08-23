056285b84e14db50b2901d8e40b54fd568b413de
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-23 12:03:28 +0430

    :pencil2: fixed productRepo typo

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { validationResult } = require("express-validator");
 const multer = require("multer");
 
-const ProductsRepo = require("../../repositories/products");
+const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("./validators");
 
