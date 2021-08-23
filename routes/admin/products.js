867bb85b81d269063f94776fae80066aca3a3d06
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-23 13:43:07 +0430

    :hammer: redirected to indexProduct after making a new product

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -30,7 +30,7 @@ router.post(
     const { title, price } = req.body;
     await productsRepo.create({ title, price, image });
 
-    res.send("submitted");
+    res.redirect("/admin/products");
   }
 );
 
