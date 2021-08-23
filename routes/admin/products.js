f0a603129948fea9a197469121c95cc0e71973e0
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-23 11:58:49 +0430

    :hammer: used multer to accept image upload from admins

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
+const multer = require("multer");
 
 const ProductsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/admin/products", (req, res) => {});
 
@@ -13,11 +15,17 @@ router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-  const errors = validationResult(req);
-  console.log(errors);
+router.post(
+  "/admin/products/new",
+  [requireTitle, requirePrice],
+  upload.single("image"),
+  (req, res) => {
+    const errors = validationResult(req);
 
-  res.send("submitted");
-});
+    console.log(req.file);
+
+    res.send("submitted");
+  }
+);
 
 module.exports = router;
