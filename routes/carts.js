538d42ae532522451de2c82ac57ae4fd57e4fcb8
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-27 15:40:29 +0430

    :hammer: made adding item to cart function

--- routes/carts.js
+++ routes/carts.js
@@ -13,7 +13,17 @@ router.post("/cart/products", async (req, res) => {
     cart = await cartsRepo.getOne(req.session.cartId);
   }
 
-  console.log(cart);
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.items.push({ id: req.body.productId, quanity: 1 });
+  }
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
 
   res.send("Product added to cart");
 });
