8755b008a5e72a32370412a8e429f41258b99119
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-27 15:29:11 +0430

    :hammer: made cart or get cart from cart repo

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,21 @@
 const express = require("express");
 
+const cartsRepo = require("../repositories/carts");
+
 const router = express.Router();
+
+router.post("/cart/products", async (req, res) => {
+  let cart;
+  if (!req.session.cartId) {
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+
+  console.log(cart);
+
+  res.send("Product added to cart");
+});
 
 module.exports = router;
