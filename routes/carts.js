93efcfa0204bff76cad0189f3678e6638ed711b2
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-27 15:51:27 +0430

    :hammer: rendered cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const cartsRepo = require("../repositories/carts");
+const productsRepo = require("../repositories/products");
+const cartShowTemplate = require("../views/carts/show");
 
 const router = express.Router();
 
@@ -26,6 +28,22 @@ router.post("/cart/products", async (req, res) => {
   });
 
   res.send("Product added to cart");
+});
+
+router.get("/cart", async (req, res) => {
+  if (!req.session.cartId) {
+    res.redirect("/");
+  }
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+
+    item.product = product;
+  }
+
+  res.send(cartShowTemplate({ items: cart.items }));
 });
 
 module.exports = router;
