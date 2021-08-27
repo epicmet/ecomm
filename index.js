f0686443b5bd188cf04af96037131f7405b0d611
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-27 15:10:56 +0430

    :hammer: started implementing shopping cart

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
 const adminProductsRouter = require("./routes/admin/products");
 const productsRouter = require("./routes/products");
+const cartsRouter = require("./routes/carts");
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(
 app.use(authRouter);
 app.use(adminProductsRouter);
 app.use(productsRouter);
+app.use(cartsRouter);
 
 app.listen(3000, () => {
   console.log("Listening on Port 3000");
